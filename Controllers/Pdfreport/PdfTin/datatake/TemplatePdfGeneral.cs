830048cfe5f0ca7da72b7ad6e05fd0bd7396a2e1
Enuar Munoz <enuarmunozcastillo@gmail.com>
2018-10-04 17:39:55 -0500

    Changes report general datatake

--- Controllers/Pdfreport/PdfTin/datatake/TemplatePdfGeneral.cs
+++ Controllers/Pdfreport/PdfTin/datatake/TemplatePdfGeneral.cs
@@ -13,7 +13,7 @@ namespace pmacore_api.Controllers.Pdfreport.PdfTin.datatake
         {
 
             var encabezado = Path.Combine(wwwroot, "Images", "logo_electrohuila.png");
-            var image = string.Format("<img  width='130'  src='{0}' />", encabezado);
+            var image = string.Format("<img  width='170'  src='{0}' />", encabezado);
             var dateNow = DateTime.Now.ToString("MM/dd/yyyy");
             //var image = string.Format("<img  width='40'  src='{0}' />", encabezado);
 
@@ -26,12 +26,6 @@ namespace pmacore_api.Controllers.Pdfreport.PdfTin.datatake
             </head>
             <body>");
 
-
-           
-
-
-
-
             foreach (var item in response.List)
             {
                 
@@ -42,30 +36,80 @@ namespace pmacore_api.Controllers.Pdfreport.PdfTin.datatake
 
                 ");
 
-                main.AppendFormat(@"<table style='width: 100%;font-size:7pt;font-family:tahoma;'>
+                main.AppendFormat(@"<table >
 													<tr>
-													<td rowspan='2' align='center'> 
+													<td rowspan='2' align='center'  width='25%'> 
 															{0}
 													</td>
 													<td colspan='2' align='center'>
-															<span style='padding:0px !important; color:#e26912; font-weight: bold;'>  
+															<span class='title'>  
 															INVENTARIO CABLE OPERADORES 
 															</span>
 													</td>
 													</tr>
 													<tr>
-													<td> 
-															<span style='color:#e26912; font-weight: bold;'>  FECHA(DD/MM/AAAA):  </span>{1}<br />
-															<span style='color:#e26912; font-weight: bold;'>  CIUDAD DE EJECUCUION:  </span><br />
-															<span style='color:#e26912; font-weight: bold;'>  PROYECTO:  </span>{2}<br />
+													<td align='left' width='50%'> 
+															<span class='title'>  FECHA(DD/MM/AAAA):  </span>{1}<br />
+															<span class='title'>  CIUDAD DE EJECUCUION:  </span><br />
+															<span class='title'>  PROYECTO:  </span>{2}<br />
 													</td>
-													<td> 
-															<span style='color:#e26912; font-weight: bold;'> EMPRESA OPERADORA  </span><br />
-															<span style='color:#e26912; font-weight: bold;'>  ORDEN DE TRABAJO  </span>01<br />
-															<span style='color:#e26912; font-weight: bold;'>  VERSION:  </span>01<br />
+													<td align='left'  width='25%'> 
+															<span class='title'>   EMPRESA OPERADORA  </span><br />
+															<span class='title'>   ORDEN DE TRABAJO  </span>01<br />
+															<span class='title'>   VERSION:  </span>01<br />
 													</td>
 													</tr>
 											</table>", image, dateNow, item.nombre_proyecto);
+
+                main.AppendFormat(@"<p></p><table>
+																		<tr >
+																			<th width='10%'><strong>Ciudad:</strong></th>
+																			<td width='20%' align='center'>{0}</td>
+																			<td width='10%'><strong>Postes</strong></td>
+																			<td width='20%' align='center'>{1}</td>
+																			<td width='20%' rowspan='2'><strong>Ocupaciones por longitud</strong></td>
+																			<td width='20%' rowspan='2' 
+																			>
+																			<strong>6 m:  </strong> {4}
+																			<br><strong>8 m:  </strong> {5}
+																			<br><strong>10 m:  </strong> {6}
+																			<br> <strong>12 m: </strong> {7}
+																			<br> <strong>14 m: </strong> {8} 
+																			<br> <strong>16 m: </strong> {9} 
+																			</td>
+																		</tr>
+																		<tr>
+																			<td width='10%'><strong>Operador:</strong></td>
+																			<td width='20%' align='center'>{2}</td>
+																			<td width='10%'><strong>Ocupaciones:</strong></td>
+																			<td width='10%' align='center'>{3}</td>
+																	
+																		</tr>
+																		</table>", 1,2,3,4,5,6,7,8,9,10);
+
+
+                                                                        main.AppendFormat(@"
+                                                                        <table style='width: 100%;font-size:7pt;font-family:tahoma;'>
+																		    <tr align='center'>
+                                                                                <th ><strong>#</strong></th>
+                                                                                <th ><strong>Numero apoyo</strong></th>
+                                                                                <th ><strong>Codigo Apoyo</strong></th>
+                                                                                <th ><strong>Long. Poste</strong></th>
+                                                                                <th ><strong>Estado</strong></th>
+                                                                                <th ><strong>Nivel Tension</strong></th>
+                                                                                <th ><strong>Altura Disponible</strong></th>
+                                                                                <th ><strong>Resistencia Mecanica</strong></th>
+                                                                                <th ><strong>Material</strong></th>
+                                                                                <th ><strong>Retenidas</strong></th>
+                                                                                <th ><strong>Direccion</strong></th>
+                                                                                <th ><strong>Cable</strong></th>
+                                                                                <th ><strong>Ocupaciones</strong></th>
+                                                                                <th ><strong>Coordenadas</strong></th>
+																			</tr>
+																		</table>");
+
+
+
 
 
                 main.Append(@"
